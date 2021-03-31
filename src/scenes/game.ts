cec13139ff573d6cbfac06bfff844e40fe011196
jfx21 <81051927+jfx21@users.noreply.github.com>
2021-03-31 15:38:55 +0200

    Update game.ts

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -379,12 +379,3 @@ private createPlayerAnims()
             })      
 }
 }
-function applyMixins(derivedCtor: any, baseCtors: any[]) {
-    baseCtors.forEach(baseCtor => {
-        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
-             if (name !== 'constructor') {
-                derivedCtor.prototype[name] = baseCtor.prototype[name];
-            }
-        });
-    }); 
-}
