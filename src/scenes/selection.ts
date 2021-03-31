a1027ae3e0a38a96c23fe13459968fba43fdd4cb
jfx21 <81051927+jfx21@users.noreply.github.com>
2021-03-31 15:41:15 +0200

    Update selection.ts

--- src/scenes/selection.ts
+++ src/scenes/selection.ts
@@ -1,5 +1,5 @@
 import Phaser from 'phaser'
-import {primaryButton} from '~/ui/button.jsx'
+import {primaryButton} from '../ui/button.jsx'
 import WebFontFile from '~/files/font.ts'
 
 export default class selection extends Phaser.Scene
